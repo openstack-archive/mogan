10e8d712cab675005bcd5191be6ed90d2756f22d
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2016-08-23 11:20:52 +0800

    Add eventlet monkey_patch()
    Change-Id: I97aa6ba633e8a8bf428dfce99ebfe5e4c947a7a0

--- nimble/__init__.py
+++ nimble/__init__.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+# Copyright 2016 Huawei Technologies Co.,LTD.
+# All Rights Reserved.
 
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -12,8 +13,10 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import pbr.version
+import os
 
+os.environ['EVENTLET_NO_GREENDNS'] = 'yes'
 
-__version__ = pbr.version.VersionInfo(
-    'nimble').version_string()
+import eventlet
+
+eventlet.monkey_patch(os=False)
