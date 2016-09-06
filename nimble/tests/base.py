c149e47b10a2d4b8a859e92ebe1e20c427523627
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2016-09-06 15:45:15 +0800

    Add base unit tests
    Change-Id: Ica54ba17dfa9a7f930d6f2f321c86aa79657878d

--- nimble/tests/base.py
+++ nimble/tests/base.py
@@ -1,23 +1,101 @@
-# -*- coding: utf-8 -*-
-
-# Copyright 2010-2011 OpenStack Foundation
-# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
-from oslotest import base
+"""Base classes for our unit tests.
+
+Allows overriding of config for use of fakes, and some black magic for
+inline callbacks.
+
+"""
+
+import os
+import tempfile
+
+import eventlet
+eventlet.monkey_patch(os=False)
+import fixtures
+from oslo_config import fixture as config_fixture
+from oslo_log import log as logging
+import testtools
+
+from nimble.common import config as nimble_config
+from nimble.conf import CONF
 
 
-class TestCase(base.BaseTestCase):
+logging.register_options(CONF)
+logging.setup(CONF, 'nimble')
 
+
+class TestCase(testtools.TestCase):
     """Test case base class for all unit tests."""
+
+    def setUp(self):
+        """Run before each test method to initialize test environment."""
+        super(TestCase, self).setUp()
+        test_timeout = os.environ.get('OS_TEST_TIMEOUT', 0)
+        try:
+            test_timeout = int(test_timeout)
+        except ValueError:
+            # If timeout value is invalid do not set a timeout.
+            test_timeout = 0
+        if test_timeout > 0:
+            self.useFixture(fixtures.Timeout(test_timeout, gentle=True))
+        self.useFixture(fixtures.NestedTempfile())
+        self.useFixture(fixtures.TempHomeDir())
+
+        if (os.environ.get('OS_STDOUT_CAPTURE') == 'True' or
+                os.environ.get('OS_STDOUT_CAPTURE') == '1'):
+            stdout = self.useFixture(fixtures.StringStream('stdout')).stream
+            self.useFixture(fixtures.MonkeyPatch('sys.stdout', stdout))
+        if (os.environ.get('OS_STDERR_CAPTURE') == 'True' or
+                os.environ.get('OS_STDERR_CAPTURE') == '1'):
+            stderr = self.useFixture(fixtures.StringStream('stderr')).stream
+            self.useFixture(fixtures.MonkeyPatch('sys.stderr', stderr))
+
+        self.log_fixture = self.useFixture(fixtures.FakeLogger())
+        self._set_config()
+
+        self.addCleanup(self._clear_attrs)
+        self.useFixture(fixtures.EnvironmentVariable('http_proxy'))
+
+    def _set_config(self):
+        self.cfg_fixture = self.useFixture(config_fixture.Config(CONF))
+        self.config(use_stderr=False,
+                    fatal_exception_format_errors=True,
+                    tempdir=tempfile.tempdir)
+        self.set_defaults(host='fake-host',
+                          debug=True)
+        self.set_defaults(connection="sqlite://",
+                          sqlite_synchronous=False,
+                          group='database')
+        nimble_config.parse_args([], default_config_files=[])
+
+    def _clear_attrs(self):
+        # Delete attributes that don't start with _ so they don't pin
+        # memory around unnecessarily for the duration of the test
+        # suite
+        for key in [k for k in self.__dict__ if k[0] != '_']:
+            del self.__dict__[key]
+
+    def config(self, **kw):
+        """Override config options for a test."""
+        self.cfg_fixture.config(**kw)
+
+    def set_defaults(self, **kw):
+        """Set default values of config options."""
+        group = kw.pop('group', None)
+        for o, v in kw.items():
+            self.cfg_fixture.set_default(o, v, group=group)
