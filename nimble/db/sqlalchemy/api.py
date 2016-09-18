874c7234f73fd83911eb5967399b40ce448486b7
Kevin_Zheng <zhengzhenyu@huawei.com>
2016-09-18 15:29:16 +0800

    WIP refactor flavor to instance_type
    This patch refactors flavor to instance_type.
    The extra_specs field will be added in follow-up
    patches.
    
    Change-Id: Ie29fc75bee0907e9f4cbd5dca73f6cd1a98258a0
    Partially-Implements: blueprint refactor-flavor-to-instance-type

--- nimble/db/sqlalchemy/api.py
+++ nimble/db/sqlalchemy/api.py
@@ -79,7 +79,7 @@ class Connection(api.Connection):
     def __init__(self):
         pass
 
-    def flavor_create(self, values):
+    def instance_type_create(self, values):
         if not values.get('uuid'):
             values['uuid'] = uuidutils.generate_uuid()
 
@@ -94,24 +94,24 @@ class Connection(api.Connection):
                 raise exception.FlavorAlreadyExists(name=values['name'])
             return instance_type
 
-    def flavor_get(self, flavor_id):
-        query = model_query(models.InstanceTypes).filter_by(uuid=flavor_id)
+    def instance_type_get(self, flavor_uuid):
+        query = model_query(models.InstanceTypes).filter_by(uuid=flavor_uuid)
         try:
             return query.one()
         except NoResultFound:
-            raise exception.FlavorNotFound(flavor=flavor_id)
+            raise exception.FlavorNotFound(flavor=flavor_uuid)
 
-    def flavor_get_all(self):
+    def instance_type_get_all(self):
         return model_query(models.InstanceTypes)
 
-    def flavor_destroy(self, flavor_id):
+    def instance_type_destroy(self, flavor_uuid):
         with _session_for_write():
             query = model_query(models.InstanceTypes)
-            query = add_identity_filter(query, flavor_id)
+            query = add_identity_filter(query, flavor_uuid)
 
             count = query.delete()
             if count != 1:
-                raise exception.FlavorNotFound(flavor=flavor_id)
+                raise exception.FlavorNotFound(flavor=flavor_uuid)
 
     def instance_create(self, values):
         if not values.get('uuid'):
