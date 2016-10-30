ffddae0d172d61bf2cf3f72bce3e9620d1f22ed4
Zhong Luyao <luyao.zhong@intel.com>
2016-10-31 05:50:35 +0800

    Use sqlalchemyutils.model_query
    Also add a new "project_only" parameters for model_query.
    
    This project_only is used for multi-tenant support.
    
    The project_only is in kwargs if pass to model_query.
    
    Co-Authored-By: LeiZhang <lei.a.zhang@intel.com>
    
    Change-Id: I54f1d10222449683ab7001d2329f377bad1df110

--- nimble/db/sqlalchemy/api.py
+++ nimble/db/sqlalchemy/api.py
@@ -19,6 +19,7 @@ import threading
 
 from oslo_db import exception as db_exc
 from oslo_db.sqlalchemy import enginefacade
+from oslo_db.sqlalchemy import utils as sqlalchemyutils
 from oslo_utils import strutils
 from oslo_utils import uuidutils
 from sqlalchemy.orm.exc import NoResultFound
@@ -50,10 +51,21 @@ def model_query(context, model, *args, **kwargs):
 
     :param context: Context of the query
     :param model: Model to query. Must be a subclass of ModelBase.
+    :param args: Arguments to query. If None - model is used.
+    """
+    """
+    project_only: In kwargs if passed it.
+      If False,then restrict query to match the context's project_id.
+      if True, then the admin user 
+                         will get related info from all projects.
     """
 
+    if kwargs.pop("project_only", False):
+        kwargs["project_id"] = context.project_id
+
     with _session_for_read() as session:
-        query = session.query(model, *args)
+        query = sqlalchemyutils.model_query(
+            model, session, args, **kwargs)
         return query
 
 
