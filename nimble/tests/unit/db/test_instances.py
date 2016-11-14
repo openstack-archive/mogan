6d7c93e56a987438165f889c52fcb85e42c6532c
Zhong Luyao <luyao.zhong@intel.com>
2016-11-14 15:50:30 +0800

    Add instance dbapi test cases
    Change-Id: I63d93d8a62a0e2f832fda17dbcee4aa434701ff6

--- nimble/tests/unit/db/test_instances.py
+++ nimble/tests/unit/db/test_instances.py
@@ -15,6 +15,10 @@
 
 """Tests for manipulating Instances via the DB API"""
 
+from oslo_utils import uuidutils
+import six
+
+from nimble.common import exception
 from nimble.tests.unit.db import base
 from nimble.tests.unit.db import utils
 
@@ -23,3 +27,88 @@ class DbInstanceTestCase(base.DbTestCase):
 
     def test_instance_create(self):
         utils.create_test_instance()
+
+    def test_instance_create_already_exist(self):
+        utils.create_test_instance()
+        self.assertRaises(exception.InstanceAlreadyExists,
+                          utils.create_test_instance)
+
+    def test_instance_create_with_same_uuid(self):
+        utils.create_test_instance(uuid='uuid', name='instance1')
+        self.assertRaises(exception.InstanceAlreadyExists,
+                          utils.create_test_instance,
+                          uuid='uuid',
+                          name='instance2')
+
+    def test_instance_create_with_duplicate_name(self):
+        utils.create_test_instance(uuid='uuid-1', name='instance')
+        self.assertRaises(exception.InstanceAlreadyExists,
+                          utils.create_test_instance,
+                          uuid='uuid-2',
+                          name='instance')
+
+    def test_instance_get_by_uuid(self):
+        instance = utils.create_test_instance()
+        res = self.dbapi.instance_get(self.context, instance.uuid)
+        self.assertEqual(instance.id, res.id)
+        self.assertEqual(instance.uuid, res.uuid)
+
+    def test_instance_get_not_exist(self):
+        self.assertRaises(exception.InstanceNotFound,
+                          self.dbapi.instance_get,
+                          self.context,
+                          '12345678-9999-0000-aaaa-123456789012')
+
+    def test_instance_get_all(self):
+        uuids = []
+        for i in range(1, 6):
+            instance = utils.create_test_instance(
+                uuid=uuidutils.generate_uuid(),
+                name=str(i))
+            uuids.append(six.text_type(instance['uuid']))
+        res = self.dbapi.instance_get_all(self.context, project_only=False)
+        res_uuids = [r.uuid for r in res]
+        six.assertCountEqual(self, uuids, res_uuids)
+
+    def test_instance_destroy(self):
+        instance = utils.create_test_instance()
+        self.dbapi.instance_destroy(self.context, instance.uuid)
+        self.assertRaises(exception.InstanceNotFound,
+                          self.dbapi.instance_get,
+                          self.context,
+                          instance.uuid)
+
+    def test_instance_destroy_not_exist(self):
+        self.assertRaises(exception.InstanceNotFound,
+                          self.dbapi.instance_destroy,
+                          self.context,
+                          '12345678-9999-0000-aaaa-123456789012')
+
+    def test_instance_update(self):
+        instance = utils.create_test_instance()
+        old_extra = instance.extra
+        new_extra = {'foo': 'bar'}
+        self.assertNotEqual(old_extra, new_extra)
+
+        res = self.dbapi.instance_update(self.context,
+                                         instance.id,
+                                         {'extra': new_extra})
+        self.assertEqual(new_extra, res.extra)
+
+    def test_instance_update_with_invalid_parameter_value(self):
+        instance = utils.create_test_instance()
+        self.assertRaises(exception.InvalidParameterValue,
+                          self.dbapi.instance_update,
+                          self.context,
+                          instance.id,
+                          {'uuid': '12345678-9999-0000-aaaa-123456789012'})
+
+    def test_instance_update_with_duplicate_name(self):
+        instance1 = utils.create_test_instance(uuid=uuidutils.generate_uuid(),
+                                               name='spam')
+        instance2 = utils.create_test_instance(uuid=uuidutils.generate_uuid())
+        self.assertRaises(exception.DuplicateName,
+                          self.dbapi.instance_update,
+                          self.context,
+                          instance2.id,
+                          {'name': instance1.name})
