367fdfa34bb000acbeb5ab3b15a9f71dc865440f
liusheng <liusheng@huawei.com>
2016-11-30 10:53:38 +0800

    Add tempest tests for instance creation(WIP)
    Change-Id: I6fc1b8b5f962ab78777821e7812a444ef1f4f7ca

--- nimble/common/neutron.py
+++ nimble/common/neutron.py
@@ -48,6 +48,7 @@ def get_client(token=None):
             params['region_name'] = CONF.keystone.region_name
     else:
         params['token'] = token
+        params['auth_url'] = CONF.keystone_authtoken.auth_url
         params['endpoint_url'] = url or keystone.get_service_url(
             session, service_type='network')
         params.update({
@@ -61,7 +62,7 @@ def get_client(token=None):
 def create_port(context, network_uuid, mac, instance_uuid):
     """Create neutron port."""
 
-    client = get_client(context.auth_token)
+    client = get_client()
     body = {
         'port': {
             'network_id': network_uuid,
