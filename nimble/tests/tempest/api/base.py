38a4ba2fff05c397575eaf47501137cdb4642063
liusheng <liusheng@huawei.com>
2016-11-30 10:53:38 +0800

    Add tempest tests for instance creation(WIP)
    Change-Id: I6fc1b8b5f962ab78777821e7812a444ef1f4f7ca

--- nimble/tests/tempest/api/base.py
+++ nimble/tests/tempest/api/base.py
@@ -13,7 +13,10 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import time
 
+from nimble.common import exception
+from tempest.common.utils import data_utils
 from tempest import config
 from tempest.lib import exceptions as lib_exc
 import tempest.test
@@ -46,6 +49,56 @@ class BaseBaremetalComputeTest(tempest.test.BaseTestCase):
         cls.type_ids = []
         cls.instance_ids = []
 
+    @classmethod
+    def create_instance(cls, wait_until_active=True):
+        types = cls.baremetal_compute_client.list_instance_types()
+        if types:
+            type_id = types[0]['uuid']
+        else:
+            type_req = {"name": data_utils.rand_name('nimble_instance_type'),
+                        "description": "nimble instance type description",
+                        'is_public': bool(data_utils.rand_int_id(0, 1))}
+            resp = cls.baremetal_compute_client.create_instance_type(
+                **type_req)
+            type_id = resp['uuid']
+        tenant_network = cls.get_tenant_network()
+        if not tenant_network:
+            raise Exception("No tenant network found!")
+        image_id = CONF.compute.image_ref
+        body = {'name': data_utils.rand_name('nimble_instance'),
+                'description': "nimble tempest instance",
+                'instance_type_uuid': type_id,
+                'image_uuid': image_id,
+                "networks": [{"uuid": tenant_network['id']}]
+                }
+        resp = cls.baremetal_compute_client.create_instance(**body)
+        cls.instance_ids.append(resp['uuid'])
+        if wait_until_active:
+            cls._wait_for_instances_status(resp['uuid'], 'active', 30, 180)
+        return resp
+
+    @classmethod
+    def _wait_for_instances_status(cls, inst_id, status,
+                                   build_interval, build_timeout):
+        """Waits for a Instance to reach a given status."""
+        inst_status = None
+        start = int(time.time())
+
+        while inst_status != status:
+            time.sleep(build_interval)
+            body = cls.baremetal_compute_client.show_instance(inst_id)
+            inst_status = body['status']
+            if inst_status == 'error' and status != 'error':
+                msg = ('Failed to provision instance %s' % inst_id)
+                raise exception.InstanceDeployFailure(msg)
+
+            if int(time.time()) - start >= build_timeout:
+                message = ('Instance %s failed to reach %s status '
+                           '(current %s) within the required time (%s s).' %
+                           (inst_id, status, inst_status,
+                            build_timeout))
+                raise lib_exc.TimeoutException(message)
+
     @staticmethod
     def cleanup_resources(method, list_of_ids):
         for resource_id in list_of_ids:
@@ -58,7 +111,6 @@ class BaseBaremetalComputeTest(tempest.test.BaseTestCase):
     def resource_cleanup(cls):
         cls.cleanup_resources(
             cls.baremetal_compute_client.delete_instance_type, cls.type_ids)
-        # TODO(liusheng)
-        # cls.cleanup_resources(cls.baremetal_compute_client.delete_instance,
-        #  cls.instance_ids)
+        cls.cleanup_resources(cls.baremetal_compute_client.delete_instance,
+                              cls.instance_ids)
         super(BaseBaremetalComputeTest, cls).resource_cleanup()
