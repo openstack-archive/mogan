d04abd8f92942f97f6c9227b52c57c55734e794f
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2016-12-02 10:24:56 +0800

    [WIP] Add engine unit tests
    Change-Id: I6bea3171855330707ccee33f5b2cfaf23ad31ab1

---
+++ nimble/tests/unit/engine/test_manager.py
@@ -0,0 +1,56 @@
+# Copyright 2016 Huawei Technologies Co.,LTD.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Test class for Nimble ManagerService."""
+
+import mock
+
+from nimble.engine.baremetal import ironic
+from nimble.engine.baremetal import ironic_states
+from nimble.engine import manager
+from nimble.tests.unit.db import base as tests_db_base
+from nimble.tests.unit.engine import mgr_utils
+from nimble.tests.unit.objects import utils as obj_utils
+
+
+@mock.patch.object(manager.EngineManager, '_refresh_cache')
+class ManageInstanceTestCase(mgr_utils.ServiceSetUpMixin,
+                             tests_db_base.DbTestCase):
+
+    @mock.patch.object(ironic, 'set_power_state')
+    def test_change_instance_power_state(self, set_power_mock,
+                                         refresh_cache_mock):
+        instance = obj_utils.create_test_instance(self.context)
+        refresh_cache_mock.side_effect = None
+        self._start_service()
+
+        self.service.set_power_state(self.context, instance,
+                                     ironic_states.POWER_ON)
+        self._stop_service()
+
+        set_power_mock.assert_called_once_with(mock.ANY, instance.node_uuid,
+                                               ironic_states.POWER_ON)
+
+    @mock.patch.object(ironic, 'get_node_states')
+    def test_get_instance__states(self, get_states_mock, refresh_cache_mock):
+        instance = obj_utils.create_test_instance(self.context)
+        get_states_mock.return_value = mock.MagicMock()
+        refresh_cache_mock.side_effect = None
+        self._start_service()
+
+        self.service.instance_states(self.context, instance)
+        self._stop_service()
+
+        get_states_mock.assert_called_once_with(mock.ANY, instance.node_uuid)
