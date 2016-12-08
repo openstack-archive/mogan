5a53c2967723f823c0d5b04676604228afee6e7c
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2016-12-08 20:11:42 +0800

    [WIP] Add create instance taskflow
    Support rollback in the process of creating an instance
    
    Co-Authored-By: Zhenguo Niu <Niu.ZGlinux@gmail.com>
    
    Implements: blueprint add-rollback-mechanism-for-creating-a-instance
    
    Change-Id: I2f373d5acee1db88ee1eb00160966ebb0977ad50

---
+++ nimble/engine/flows/create_instance.py
@@ -0,0 +1,185 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import traceback
+
+from oslo_concurrency import processutils
+from oslo_config import cfg
+from oslo_log import log as logging
+from oslo_utils import timeutils
+import taskflow.engines
+from taskflow.patterns import linear_flow
+from taskflow.types import failure as ft
+
+LOG = logging.getLogger(__name__)
+
+ACTION = 'instance:create'
+CONF = cfg.CONF
+
+
+class ScheduleCreateInstanceTask(flow_utils.NimbleTask):
+    """Activates a scheduler driver and handles any subsequent failures."""
+
+    def __init__(self, manager):
+        super(ScheduleCreateInstanceTask, self).__init__(addons=[ACTION])
+        self.manager = manager
+
+    def execute(self, context, instance, request_spec, filter_properties):
+        try:
+            top_node = self.manager.scheduler.schedule(context,
+                                                       request_spec,
+                                                       self.node_cache,
+                                                       filter_properties)
+        except exception.NoValidNode:
+            self.manager._set_instance_obj_error_state(context, instance)
+            raise exception.NoValidNode(
+                _('No valid node is found with request spec %s') %
+                request_spec)
+        instance.node_uuid = top_node.to_dict()['node']
+
+
+class SetInstanceInfoTask(flow_utils.NimbleTask):
+    """Set instance info to ironic node and validate it."""
+
+    def __init__(self, manager):
+        super(SetInstanceInfoTask, self).__init__(addons=[ACTION])
+        self.manager = manager
+        self.ironicclient = manager.ironicclient
+
+    def execute(self, context, instance):
+        ironic.set_instance_info(self.ironicclient, instance)
+        # validate we are ready to do the deploy
+        validate_chk = ironic.validate_node(self.ironicclient,
+                                            instance.node_uuid)
+        if (not validate_chk.deploy.get('result')
+                or not validate_chk.power.get('result')):
+            self._set_instance_obj_error_state(context, instance)
+            raise exception.ValidationError(_(
+                "Ironic node: %(id)s failed to validate."
+                " (deploy: %(deploy)s, power: %(power)s)")
+                % {'id': instance.node_uuid,
+                   'deploy': validate_chk.deploy,
+                   'power': validate_chk.power})
+
+    def revert(self, context, instance, result, **kwargs):
+        # Do we need to clean up ironic node instance info?
+
+
+class BuildNetworkTask(flow_utils.NimbleTask):
+    """Build network for the instance."""
+
+    def __init__(self, manager):
+        super(BuildNetworkTask, self).__init__(addons=[ACTION])
+        self.manager = manager
+        self.ironicclient = manager.ironicclient
+
+    def _build_networks(self, context, instance, requested_networks):
+        node_uuid = instance.node_uuid
+        ironic_ports = ironic.get_ports_from_node(self.ironicclient,
+                                                  node_uuid,
+                                                  detail=True)
+        LOG.debug(_('Find ports %(ports)s for node %(node)s') %
+                  {'ports': ironic_ports, 'node': node_uuid})
+        if len(requested_networks) > len(ironic_ports):
+            raise exception.InterfacePlugException(_(
+                "Ironic node: %(id)s virtual to physical interface count"
+                "  mismatch"
+                " (Vif count: %(vif_count)d, Pif count: %(pif_count)d)")
+                % {'id': instance.node_uuid,
+                   'vif_count': len(requested_networks),
+                   'pif_count': len(ironic_ports)})
+
+        network_info = {}
+        for vif in requested_networks:
+            for pif in ironic_ports:
+                # Match the specified port type with physical interface type
+                if vif.get('port_type') == pif.extra.get('port_type'):
+                    port = neutron.create_port(context, vif['uuid'],
+                                               pif.address, instance.uuid)
+                    port_dict = port['port']
+                    network_info[port_dict['id']] = {
+                        'network': port_dict['network_id'],
+                        'mac_address': port_dict['mac_address'],
+                        'fixed_ips': port_dict['fixed_ips']}
+                    ironic.plug_vif(self.ironicclient, pif.uuid,
+                                    port_dict['id'])
+
+        return network_info
+
+
+    def execute(self, context, instance, requested_networks):
+        # Build networks
+
+    def revert(self, context, instance, result, **kwargs):
+        # Clean up networks?
+
+
+class CreateInstanceTask(flow_utils.NimbleTask):
+    """Set instance info to ironic node and validate it."""
+
+    def __init__(self, manager):
+        super(CreateInstanceTask, self).__init__(addons=[ACTION])
+        self.manager = manager
+        self.ironicclient = manager.ironicclient
+
+    def execute(self, context, instance):
+
+    def revert(self, context, instance, result, **kwargs):
+        # Do we need to clean up ironic node instance info?
+
+
+class OnFailureRescheduleTask(flow_utils.NimbleTask):
+    """Triggers a rescheduling request to be sent when reverting occurs.
+
+    If rescheduling doesn't occur this task errors out the volume.
+
+    Reversion strategy: Triggers the rescheduling mechanism whereby a cast gets
+    sent to the scheduler rpc api to allow for an attempt X of Y for scheduling
+    this volume elsewhere.
+    """
+
+
+def get_flow(context, manager, instance, requested_networks, request_spec,
+             filter_properties):
+
+    """Constructs and returns the manager entrypoint flow.
+
+    This flow will do the following:
+
+    1. Schedule a node to create instance
+    2. Set instance info to ironic node and validate it's ready to deploy
+    3. Build networks for the instance and set port id back to ironic port
+    4. Provision ironic node.
+    """
+
+    flow_name = ACTION.replace(":", "_") + "_manager"
+    instance_flow = linear_flow.Flow(flow_name)
+
+    # This injects the initial starting flow values into the workflow so that
+    # the dependency order of the tasks provides/requires can be correctly
+    # determined.
+    create_what = {
+        'context': context,
+        'filter_properties': filter_properties,
+        'request_spec': request_spec,
+        'instance': instance,
+        'requested_networks': requested_networks
+    }
+
+    instance_flow.add(ScheduleCreateInstanceTask(manager),
+                      OnFailureRescheduleTask(manager)
+                      SetInstanceInfoTask(manager),
+                      BuildNetworkTask(manager), 
+                      CreateInstanceTask(manager))
+
+    # Now load (but do not run) the flow using the provided initial data.
+    return taskflow.engines.load(instance_flow, store=create_what)
