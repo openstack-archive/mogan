b30a38ab47f7c9faf585afa54f3a3e813f2a0583
liusheng <liusheng@huawei.com>
2016-12-27 20:19:22 +0800

    Add versioned object notification base
    This change add the infra of versioned object notification.
    
    Change-Id: Ife773734511b58c8d21acc4de59867cb9ece8038
    Partially-Implements: bp support-versioned-notification

--- nimble/tests/base.py
+++ nimble/tests/base.py
@@ -13,6 +13,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import datetime
 import mock
 import os
 
@@ -21,9 +22,12 @@ from oslo_config import fixture as config_fixture
 from oslo_context import context
 from oslo_db import options
 from oslo_log import log
+from oslo_serialization import jsonutils
 from oslotest import base
 import pecan
+import six
 import testscenarios
+import testtools
 
 from nimble.common import config as nimble_config
 from nimble.tests import policy_fixture
@@ -121,3 +125,65 @@ class TestCase(base.BaseTestCase):
         """Override CONF variables for a test."""
         for k, v in kw.items():
             self.override_config(k, v)
+
+    def assertJsonEqual(self, expected, observed):
+        """Asserts that 2 complex data structures are json equivalent.
+
+        We use data structures which serialize down to json throughout
+        the code, and often times we just need to know that these are
+        json equivalent. This means that list order is not important,
+        and should be sorted.
+
+        Because this is a recursive set of assertions, when failure
+        happens we want to expose both the local failure and the
+        global view of the 2 data structures being compared. So a
+        MismatchError which includes the inner failure as the
+        mismatch, and the passed in expected / observed as matchee /
+        matcher.
+
+        """
+        if isinstance(expected, six.string_types):
+            expected = jsonutils.loads(expected)
+        if isinstance(observed, six.string_types):
+            observed = jsonutils.loads(observed)
+
+        def sort_key(x):
+            if isinstance(x, (set, list)) or isinstance(x, datetime.datetime):
+                return str(x)
+            if isinstance(x, dict):
+                items = ((sort_key(key), sort_key(value))
+                         for key, value in x.items())
+                return sorted(items)
+            return x
+
+        def inner(expected, observed):
+            if isinstance(expected, dict) and isinstance(observed, dict):
+                self.assertEqual(len(expected), len(observed))
+                expected_keys = sorted(expected)
+                observed_keys = sorted(observed)
+                self.assertEqual(expected_keys, observed_keys)
+
+                for key in list(six.iterkeys(expected)):
+                    inner(expected[key], observed[key])
+            elif (isinstance(expected, (list, tuple, set)) and
+                  isinstance(observed, (list, tuple, set))):
+                self.assertEqual(len(expected), len(observed))
+
+                expected_values_iter = iter(sorted(expected, key=sort_key))
+                observed_values_iter = iter(sorted(observed, key=sort_key))
+
+                for i in range(len(expected)):
+                    inner(next(expected_values_iter),
+                          next(observed_values_iter))
+            else:
+                self.assertEqual(expected, observed)
+
+        try:
+            inner(expected, observed)
+        except testtools.matchers.MismatchError as e:
+            inner_mismatch = e.mismatch
+            # inverting the observed / expected because testtools
+            # error messages assume expected is second. Possibly makes
+            # reading the error messages less confusing.
+            raise testtools.matchers.MismatchError(
+                observed, expected, inner_mismatch, verbose=True)
