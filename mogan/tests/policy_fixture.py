a4f9a9c39fa450e98716e0748f5f5828ec95f5aa
sxmatch <sxmatch1986@gmail.com>
2016-12-30 11:33:01 +0800

    Rename nimble to mogan(part three)
    Today we decided to rename Nimble project
    since it's same as a Cinder driver name.
    
    The new name of this project is called Mogan.
    
    For convenience of reviewing, this is a part one
    which includes codes folder named mogan.
    
    Change-Id: I919f17a769baee44ce19f65bab900f9d6bd8b982

---
+++ mogan/tests/policy_fixture.py
@@ -0,0 +1,43 @@
+# Copyright 2016 Intel
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+
+import fixtures
+from oslo_config import cfg
+from oslo_policy import opts as policy_opts
+
+from mogan.common import policy as nimble_policy
+
+CONF = cfg.CONF
+
+policy_data = """
+{
+
+}
+"""
+
+
+class PolicyFixture(fixtures.Fixture):
+    def setUp(self):
+        super(PolicyFixture, self).setUp()
+        self.policy_dir = self.useFixture(fixtures.TempDir())
+        self.policy_file_name = os.path.join(self.policy_dir.path,
+                                             'policy.json')
+        with open(self.policy_file_name, 'w') as policy_file:
+            policy_file.write(policy_data)
+        policy_opts.set_defaults(CONF)
+        CONF.set_override('policy_file', self.policy_file_name, 'oslo_policy')
+        nimble_policy._ENFORCER = None
+        self.addCleanup(nimble_policy.get_enforcer().clear)
