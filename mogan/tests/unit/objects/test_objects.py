c8514d3324c894ace70728ee57f7a47515d283fc
Zhong Luyao <luyao.zhong@intel.com>
2017-01-17 16:51:39 +0800

    Add fault_info field for object Instance
    add fault_info for Instance
    
    Change-Id: I6fb5cde7e5752683facf6ce7375c354a19cfd81a

--- mogan/tests/unit/objects/test_objects.py
+++ mogan/tests/unit/objects/test_objects.py
@@ -382,7 +382,7 @@ class _TestObject(object):
 # version bump. It is md5 hash of object fields and remotable methods.
 # The fingerprint values should only be changed if there is a version bump.
 expected_object_fingerprints = {
-    'Instance': '1.0-c3a73e3ec189aa09dc430b389c81b11f',
+    'Instance': '1.0-2666e60a102d923e2237b6a2770da90d',
     'InstanceType': '1.0-589b096651fcdb30898ff50f748dd948',
     'MyObj': '1.1-aad62eedc5a5cc8bcaf2982c285e753f',
     'FakeNode': '1.0-07813a70fee67557d8a71ad96f31cee7',
