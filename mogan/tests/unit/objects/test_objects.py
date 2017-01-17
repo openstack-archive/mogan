3a49d2000f610f88221fd50d2c73ab7d7f635039
Zhong Luyao <luyao.zhong@intel.com>
2017-01-17 16:51:39 +0800

    Add fault object
    add fault object ，it is a optional object_field of instance.
    if error occured, we can save related message into fault.
    
    Change-Id: I6fb5cde7e5752683facf6ce7375c354a19cfd81a

--- mogan/tests/unit/objects/test_objects.py
+++ mogan/tests/unit/objects/test_objects.py
@@ -383,6 +383,8 @@ class _TestObject(object):
 # The fingerprint values should only be changed if there is a version bump.
 expected_object_fingerprints = {
     'Instance': '1.0-51d866ba4ccec2e2e17ead134ad13e95',
+    'InstanceFault': '1.0-6b5b01b2cc7b6b547837acb168ec6eb9',
+    'InstanceFaultList': '1.0-1db40d8303f67f068528155b68355ce1',
     'InstanceType': '1.0-589b096651fcdb30898ff50f748dd948',
     'MyObj': '1.1-aad62eedc5a5cc8bcaf2982c285e753f',
     'FakeNode': '1.0-07813a70fee67557d8a71ad96f31cee7',
