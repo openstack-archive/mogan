12a327433a1d53223271c7202f9513b20059b2d9
wanghao <sxmatch1986@gmail.com>
2017-03-09 15:53:38 +0800

    Support quota in mogan(part two)
    This patch introduces the quota operation in
    creating and deleting instances process.
    
    Change-Id: If6573fafc5acb805cf727acdc8f0f9872fc1a717
    Implements: blueprint quota-support

--- mogan/engine/manager.py
+++ mogan/engine/manager.py
@@ -35,6 +35,7 @@ from mogan.engine.flows import create_instance
 from mogan.notifications import base as notifications
 from mogan import objects
 from mogan.objects import fields
+from mogan.objects import quota
 
 LOG = log.getLogger(__name__)
 
@@ -46,6 +47,11 @@ class EngineManager(base_manager.BaseEngineManager):
 
     target = messaging.Target(version=RPC_API_VERSION)
     _lock = threading.Lock()
+
+    def __init__(self, *args, **kwargs):
+        super(EngineManager, self).__init__(*args, **kwargs)
+        self.quota = quota.Quota()
+        self.quota.register_resource(objects.quota.InstanceResource())
 
     def _refresh_cache(self):
         node_cache = {}
@@ -244,6 +250,12 @@ class EngineManager(base_manager.BaseEngineManager):
         for pif in bm_interface:
             self.driver.unplug_vif(pif)
 
+    def _rollback_instances_quota(self, context, number):
+        reserve_opts = {'instances': number}
+        reservations = self.quota.reserve(context, **reserve_opts)
+        if reservations:
+            self.quota.commit(context, reservations)
+
     def create_instance(self, context, instance, requested_networks,
                         request_spec=None, filter_properties=None):
         """Perform a deployment."""
@@ -270,6 +282,7 @@ class EngineManager(base_manager.BaseEngineManager):
                 filter_properties,
             )
         except Exception:
+            self._rollback_instances_quota(context, -1)
             msg = _("Create manager instance flow failed.")
             LOG.exception(msg)
             raise exception.MoganException(msg)
@@ -289,6 +302,7 @@ class EngineManager(base_manager.BaseEngineManager):
             instance.power_state = states.NOSTATE
             instance.status = fsm.current_state
             instance.save()
+            self._rollback_instances_quota(context, -1)
             LOG.error(_LE("Created instance %(uuid)s failed."
                           "Exception: %(exception)s"),
                       {"uuid": instance.uuid,
@@ -344,6 +358,7 @@ class EngineManager(base_manager.BaseEngineManager):
                     instance.power_state = states.NOSTATE
                     instance.status = fsm.current_state
                     instance.save()
+                    self._rollback_instances_quota(context, 1)
 
         do_delete_instance(instance)
 
