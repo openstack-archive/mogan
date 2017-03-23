7a341d8e04a343d1f30a340b417086e73e590f5d
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2017-03-23 15:21:05 +0800

    Move schedule task out of create instance flow
    For multi instance creation scenario, we need to schedule for all
    requested instances first then run the flow separately, so better
    to move this task out, and leave the OnFailureReschedule task to
    the flow.
    
    Change-Id: If576003cd6a2db0dd90e2ee5cdf0b0cb00f6da63

--- mogan/engine/manager.py
+++ mogan/engine/manager.py
@@ -327,6 +327,19 @@ class EngineManager(base_manager.BaseEngineManager):
             filter_properties = {}
 
         try:
+            node = self.scheduler_rpcapi.select_destinations(
+                context, request_spec, filter_properties)
+            instance.node_uuid = node['node_uuid']
+            instance.save()
+        except Exception as e:
+            utils.process_event(fsm, instance, event='error')
+            LOG.error(_LE("Created instance %(uuid)s failed."
+                          "Exception: %(exception)s"),
+                      {"uuid": instance.uuid,
+                       "exception": e})
+            return
+
+        try:
             flow_engine = create_instance.get_flow(
                 context,
                 self,
@@ -336,6 +349,7 @@ class EngineManager(base_manager.BaseEngineManager):
                 filter_properties,
             )
         except Exception:
+            utils.process_event(fsm, instance, event='error')
             msg = _("Create manager instance flow failed.")
             LOG.exception(msg)
             raise exception.MoganException(msg)
