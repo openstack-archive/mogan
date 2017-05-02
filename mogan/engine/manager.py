d87802bb9a8c467056908e3a4ab558e6bd006624
Xinran WANG <xin-ran.wang@intel.com>
2017-05-02 20:31:39 +0800

    Detach interface for server
    Change-Id: Ib474e542a4d9c32bb15f91d0993e0f6dd990ca8a

--- mogan/engine/manager.py
+++ mogan/engine/manager.py
@@ -551,3 +551,17 @@ class EngineManager(base_manager.BaseEngineManager):
                 'host': parsed_url.hostname,
                 'port': parsed_url.port,
                 'internal_access_path': None}
+
+    def detach_interface(self, context, server, port_id):
+        LOG.debug('Detaching interface', server=server)
+        try:
+            self.network_api.detach_neutron_port(context, server, port_id)
+        except Exception:
+            msg = _('Detach neutron port %(port_id) failed'
+                    ) % ({'port_id': port_id})
+            LOG.exception(msg)
+        try:
+            self.driver.unplug_vifs(context, server)
+        except Exception:
+            LOG.exception('Detach ironic port failed')
+        LOG.info('Interface was successfully detached', server=server)
