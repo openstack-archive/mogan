e592552286095fb60ae6a9a51a9ec4dc67b685a0
liusheng <liusheng@huawei.com>
2017-05-09 19:24:31 +0800

    WIP: Add support for scheduler_hints
    Change-Id: I7c482ca0ac1031f0017c6bc832892111ecfb20dc

--- mogan/api/controllers/v1/schemas/servers.py
+++ mogan/api/controllers/v1/schemas/servers.py
@@ -16,34 +16,45 @@
 
 from mogan.api.validation import parameter_types
 
-
 create_server = {
     "type": "object",
     "properties": {
-        'name': parameter_types.name,
-        'description': parameter_types.description,
-        'availability_zone': parameter_types.availability_zone,
-        'image_uuid': parameter_types.image_id,
-        'flavor_uuid': parameter_types.flavor_id,
-        'networks': {
-            'type': 'array', 'minItems': 1,
-            'items': {
-                'type': 'object',
-                'properties': {
-                    'net_id': parameter_types.network_id,
-                    'port_type': parameter_types.port_type,
+        "server": {
+            "type": "object",
+            "properties": {
+                'name': parameter_types.name,
+                'description': parameter_types.description,
+                'availability_zone': parameter_types.availability_zone,
+                'image_uuid': parameter_types.image_id,
+                'flavor_uuid': parameter_types.flavor_id,
+                'networks': {
+                    'type': 'array', 'minItems': 1,
+                    'items': {
+                        'type': 'object',
+                        'properties': {
+                            'net_id': parameter_types.network_id,
+                            'port_type': parameter_types.port_type,
+                        },
+                        'required': ['net_id'],
+                        'additionalProperties': False,
+                    },
                 },
-                'required': ['net_id'],
-                'additionalProperties': False,
+                'user_data': {'type': 'string', 'format': 'base64'},
+                'personality': parameter_types.personality,
+                'key_name': parameter_types.name,
+                'min_count': {'type': 'integer', 'minimum': 1},
+                'max_count': {'type': 'integer', 'minimum': 1},
+                'extra': parameter_types.extra,
             },
+            'required': ['name', 'image_uuid', 'flavor_uuid', 'networks'],
+            'additionalProperties': False,
         },
-        'user_data': {'type': 'string', 'format': 'base64'},
-        'personality': parameter_types.personality,
-        'key_name': parameter_types.name,
-        'min_count': {'type': 'integer', 'minimum': 1},
-        'max_count': {'type': 'integer', 'minimum': 1},
-        'extra': parameter_types.extra,
+        "scheduler_hints": {
+            'type': 'object',
+            'properties': {},
+            'additionalProperties': True,
+        }
     },
-    'required': ['name', 'image_uuid', 'flavor_uuid', 'networks'],
+    'required': ['server'],
     'additionalProperties': False,
 }
