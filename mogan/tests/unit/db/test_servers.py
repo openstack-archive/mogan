aab623ed91650523702763e216ddc4e86903d351
Xinran <xin-ran.wang@intel.com>
2017-05-27 10:21:49 +0800

    Add filters to server list API
    APIImpact
    DocImpact
    
    Closes-Bug: #1697390
    Change-Id: I1fd41d0e1897d0187e49cb54c7d4539ce64a70ff

--- mogan/tests/unit/db/test_servers.py
+++ mogan/tests/unit/db/test_servers.py
@@ -47,35 +47,66 @@ class DbServerTestCase(base.DbTestCase):
                           '12345678-9999-0000-aaaa-123456789012')
 
     def test_server_get_all(self):
-        uuids_project_1 = []
-        uuids_project_2 = []
-        uuids_project_all = []
+        servers_project_1 = []
+        servers_project_2 = []
+        servers_project_all = []
         for i in range(0, 3):
             server = utils.create_test_server(
                 uuid=uuidutils.generate_uuid(),
                 project_id='project_1',
                 name=str(i))
-            uuids_project_1.append(six.text_type(server['uuid']))
+            servers_project_1.append(server)
         for i in range(3, 5):
             server = utils.create_test_server(
                 uuid=uuidutils.generate_uuid(),
                 project_id='project_2',
                 name=str(i))
-            uuids_project_2.append(six.text_type(server['uuid']))
-        uuids_project_all.extend(uuids_project_1)
-        uuids_project_all.extend(uuids_project_2)
+            servers_project_2.append(server)
+        servers_project_all.extend(servers_project_1)
+        servers_project_all.extend(servers_project_2)
 
         # Set project_only to False
         # get all servers from all projects
         res = self.dbapi.server_get_all(self.context, project_only=False)
-        res_uuids = [r.uuid for r in res]
-        six.assertCountEqual(self, uuids_project_all, res_uuids)
+        for i, item in enumerate(res):
+            self.assertEqual(servers_project_all[i].uuid, item.uuid)
+
+        # Filter by server name test
+        res = self.dbapi.server_get_all(self.context, project_only=False,
+                                        filters={"name": "1"})
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res[0].name, "1")
+
+        # Filter by server status test
+        res = self.dbapi.server_get_all(self.context, project_only=False,
+                                        filters={"status": "active"})
+        for i, item in enumerate(res):
+            self.assertEqual(item.status, "active")
+
+        # Filter by flavor_uuid test
+        res = self.dbapi.server_get_all(self.context, project_only=False,
+                                        filters={"flavor_uuid":
+                                                 servers_project_all[0].
+                                                 flavor_uuid})
+        for i, item in enumerate(res):
+            self.assertEqual(item.flavor_uuid,
+                             servers_project_all[0].flavor_uuid)
+
+        # Filter by image_uuid test
+        res = self.dbapi.server_get_all(self.context, project_only=False,
+                                        filters={"image_uuid":
+                                                 servers_project_all[0].
+                                                 image_uuid})
+        for i, item in enumerate(res):
+            self.assertEqual(item.image_uuid,
+                             servers_project_all[0].image_uuid)
 
         # Set project_only to True
         # get servers from current project (project_1)
         self.context.tenant = 'project_1'
         res = self.dbapi.server_get_all(self.context, project_only=True)
         res_uuids = [r.uuid for r in res]
+        uuids_project_1 = [r.uuid for r in servers_project_1]
         six.assertCountEqual(self, uuids_project_1, res_uuids)
 
         # Set project_only to True
@@ -83,6 +114,7 @@ class DbServerTestCase(base.DbTestCase):
         self.context.tenant = 'project_2'
         res = self.dbapi.server_get_all(self.context, project_only=True)
         res_uuids = [r.uuid for r in res]
+        uuids_project_2 = [r.uuid for r in servers_project_2]
         six.assertCountEqual(self, uuids_project_2, res_uuids)
 
     def test_server_destroy(self):
