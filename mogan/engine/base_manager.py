c6b3eab72cc1a8a6e49f43e569c4fef5c1c87ba1
liusheng <liusheng@huawei.com>
2017-06-26 11:10:55 +0800

    Refactor the scheduler to use placement service
    This change change the scheduler to use placement api to filter query
    nodes with specified resource class of flavor, and clean the filters
    and weighers in scheduler.
    
    Change-Id: I89ad443f553510da6daf289b83f3c30d9d546ace
    Partially Implements: bp track-resources-using-placement

--- mogan/engine/base_manager.py
+++ mogan/engine/base_manager.py
@@ -24,7 +24,6 @@ from mogan.db import api as dbapi
 from mogan.engine.baremetal import driver
 from mogan.engine import rpcapi
 from mogan import network
-from mogan.scheduler import rpcapi as scheduler_rpcapi
 
 
 class BaseEngineManager(periodic_task.PeriodicTasks):
@@ -36,7 +35,6 @@ class BaseEngineManager(periodic_task.PeriodicTasks):
         self.host = host
         self.topic = topic
         self.network_api = network.API()
-        self.scheduler_rpcapi = scheduler_rpcapi.SchedulerAPI()
         self.driver = driver.load_engine_driver(CONF.engine.engine_driver)
         self.engine_rpcapi = rpcapi.EngineAPI()
         self._sync_power_pool = greenpool.GreenPool(
