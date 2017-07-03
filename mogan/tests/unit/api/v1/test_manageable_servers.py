9e5c740a083bc089e57d0e77b0cd101911a2e029
wanghao <sxmatch1986@gmail.com>
2017-07-03 14:28:04 +0800

    Manage existing BMs: Part-1
    This patch introduce a new API:
    'GET: /manageable_servers' to list the adoptable nodes
    from drivers to operators.
    
    As a reference, now we implement api in the Ironic driver.
    
    APIImpact
    
    Implements: bp manage-existing-bms
    
    Change-Id: I56340ce534c3b8d4e855a4c753ecf90a07147d29

---
+++ mogan/tests/unit/api/v1/test_manageable_servers.py
@@ -0,0 +1,44 @@
+#
+# Copyright 2017 Intel
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import mock
+
+from mogan.tests.functional.api import v1 as v1_test
+
+
+class TestManageableServers(v1_test.APITestV1):
+
+    DENY_MESSAGE = "Access was denied to the following resource: mogan:%s"
+
+    def setUp(self):
+        super(TestManageableServers, self).setUp()
+        self.project_id = "0abcdef1-2345-6789-abcd-ef123456abc1"
+        # evil_project is an wicked tenant, is used for unauthorization test.
+        self.evil_project = "0abcdef1-2345-6789-abcd-ef123456abc9"
+
+    def test_server_get_manageable_servers_with_invalid_rule(self):
+        self.context.tenant = self.evil_project
+        headers = self.gen_headers(self.context, roles="no-admin")
+        resp = self.get_json('/manageable_servers', True, headers=headers)
+        error = self.parser_error_body(resp)
+        self.assertEqual(self.DENY_MESSAGE % 'manageable_servers:get_all',
+                         error['faultstring'])
+
+    @mock.patch('mogan.engine.api.API.get_manageable_servers')
+    def test_server_get_manageable_servers(self, mock_get):
+        mock_get.return_value = mock.MagicMock()
+        self.context.tenant = self.project_id
+        headers = self.gen_headers(self.context, roles="admin")
+        self.get_json('/manageable_servers', headers=headers, status=200)
