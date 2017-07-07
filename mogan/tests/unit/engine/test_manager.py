7f5b3ac5fcfdb6ecc636f2d4bde0ff1a75fe9eda
Tao Li <litao3721@126.com>
2017-07-07 16:56:38 +0800

    Manage existing BMs: Part-2
    Add the adopt baremetal node API.
    
    Change-Id: I1897129930bface6a6c4a179e02d6107ff3811af
    Implements: bp manage-existing-bms

--- mogan/tests/unit/engine/test_manager.py
+++ mogan/tests/unit/engine/test_manager.py
@@ -24,8 +24,10 @@ from mogan.baremetal.ironic import IronicDriver
 from mogan.common import exception
 from mogan.common import ironic
 from mogan.common import states
+from mogan.engine import api as engine_api
 from mogan.engine import manager
 from mogan.network import api as network_api
+from mogan.scheduler.client.report import SchedulerReportClient as report_api
 from mogan.tests.unit.db import base as tests_db_base
 from mogan.tests.unit.engine import mgr_utils
 from mogan.tests.unit.objects import utils as obj_utils
@@ -218,3 +220,216 @@ class ManageServerTestCase(mgr_utils.ServiceSetUpMixin,
         self.service.get_manageable_servers(self.context)
         self._stop_service()
         get_manageable_mock.assert_called_once()
+
+    @mock.patch.object(manager.EngineManager, '_rollback_servers_quota')
+    @mock.patch.object(IronicDriver, 'unadopt')
+    @mock.patch.object(IronicDriver, 'adopt')
+    @mock.patch.object(network_api.API, 'show_port')
+    @mock.patch.object(report_api, 'put_allocations')
+    @mock.patch.object(IronicDriver, 'get_manageable_node')
+    def test_manage_servers(self, get_manageable_mock,
+                            put_allocations_mock, show_port_mock,
+                            adopt_mock, unadopt_mock, rollback_quota_mock):
+        neutron_port_id = '67ec8e86-d77b-4729-b11d-a009864d289d'
+        neutron_mac_address = '52:54:00:8e:6a:03'
+        node_uuid = 'aacdbd78-d670-409e-95aa-ecfcfb94fee2'
+        image_uuid = 'efe0a06f-ca95-4808-b41e-9f55b9c5eb98'
+
+        node = {
+            'uuid': node_uuid,
+            'name': 'test_manageable_mode',
+            'resource_class': 'gold',
+            'power_state': 'power on',
+            'provision_state': 'active',
+            "ports": [
+                {
+                    "address": neutron_mac_address,
+                    "uuid": "1ec01153-685a-49b5-a6d3-45a4e7dddf53",
+                    "neutron_port_id": neutron_port_id
+                }
+            ],
+            "portgroups": [
+                {
+                    "address": "a4:dc:be:0e:82:a6",
+                    "uuid": "1ec01153-685a-49b5-a6d3-45a4e7dddf54",
+                    "neutron_port_id": None
+                }
+            ],
+            'image_source': image_uuid
+        }
+
+        get_manageable_mock.return_value = node
+        put_allocations_mock.side_effect = None
+        show_port_mock.return_value = {
+            'id': neutron_port_id,
+            'network_id': '34ec8e86-d77b-4729-b11d-a009864d3456',
+            'mac_address': neutron_mac_address,
+            'fixed_ips': [{"subnet_id": "d2d7a7c2-17d2-4268-906d-1da8dde24fa8",
+                           "ip_address": "10.80.20.12"}]
+        }
+
+        server = obj_utils.get_test_server(
+            self.context, status=None, node_uuid=None,
+            power_state=states.NOSTATE, availability_zone=None,
+            image_uuid=None)
+
+        adopt_mock.side_effect = None
+        unadopt_mock.side_effect = None
+        self.service.manage_server(self.context, server, node_uuid)
+
+        put_allocations_mock.assert_called_once()
+        rollback_quota_mock.assert_not_called()
+        adopt_mock.assert_called_once()
+        unadopt_mock.assert_not_called()
+        show_port_mock.assert_called_once_with(self.context, neutron_port_id)
+        self.assertEqual(server.node_uuid, node_uuid)
+        self.assertIsNone(server.availability_zone, None)
+        self.assertEqual(server.status, 'active')
+        self.assertEqual(server.power_state, 'power on')
+        self.assertEqual(server.image_uuid, image_uuid)
+
+    @mock.patch.object(manager.EngineManager, '_rollback_servers_quota')
+    @mock.patch.object(IronicDriver, 'unadopt')
+    @mock.patch.object(IronicDriver, 'adopt')
+    @mock.patch.object(network_api.API, 'show_port')
+    @mock.patch.object(report_api, 'put_allocations')
+    @mock.patch.object(IronicDriver, 'get_manageable_node')
+    def test_manage_servers_with_mac_exception(self, get_manageable_mock,
+                                               put_allocations_mock,
+                                               show_port_mock,
+                                               adopt_mock,
+                                               unadopt_mock,
+                                               rollback_quota_mock):
+        neutron_port_id1 = '67ec8e86-d77b-4729-b11d-a009864d289d'
+        neutron_port_id2 = '67ec8e86-d77b-4729-b11d-a009864d289d'
+        neutron_mac_address1 = '52:54:00:8e:6a:03'
+        neutron_mac_address2 = '52:54:00:8e:6a:04'
+        node_uuid = 'aacdbd78-d670-409e-95aa-ecfcfb94fee2'
+
+        node = {
+            'uuid': node_uuid,
+            'name': 'test_manageable_mode',
+            'resource_class': 'gold',
+            'power_state': 'power on',
+            'provision_state': 'active',
+            "ports": [
+                {
+                    "address": neutron_mac_address1,
+                    "uuid": "1ec01153-685a-49b5-a6d3-45a4e7dddf53",
+                    "neutron_port_id": neutron_port_id1
+                }
+            ],
+            "portgroups": [
+                {
+                    "address": "a4:dc:be:0e:82:a6",
+                    "uuid": "1ec01153-685a-49b5-a6d3-45a4e7dddf54",
+                    "neutron_port_id": neutron_port_id2
+                }
+            ],
+            'image_source': 'efe0a06f-ca95-4808-b41e-9f55b9c5eb98'
+        }
+
+        get_manageable_mock.return_value = node
+        put_allocations_mock.side_effect = None
+        show_port_mock.return_value = {
+            'id': neutron_port_id1,
+            'network_id': '34ec8e86-d77b-4729-b11d-a009864d3456',
+            'mac_address': neutron_mac_address2,
+            'fixed_ips': [{"subnet_id": "d2d7a7c2-17d2-4268-906d-1da8dde24fa8",
+                           "ip_address": "10.80.20.12"}]
+        }
+
+        server = obj_utils.get_test_server(
+            self.context, status=None, node_uuid=None,
+            power_state=states.NOSTATE, availability_zone=None,
+            image_uuid=None)
+
+        adopt_mock.side_effect = None
+        self.assertRaises(exception.NetworkError, self.service.manage_server,
+                          self.context, server, node_uuid)
+
+        put_allocations_mock.assert_called_once()
+        show_port_mock.assert_called_with(self.context, neutron_port_id1)
+        show_port_mock.assert_called_with(self.context, neutron_port_id2)
+        rollback_quota_mock.assert_called_once_with(self.context, -1)
+        adopt_mock.assert_not_called()
+        unadopt_mock.assert_not_called()
+        self.assertNotEqual(server.node_uuid, node_uuid)
+        self.assertIsNone(server.availability_zone, None)
+        self.assertIsNone(server.status, None)
+        self.assertEqual(server.power_state, states.NOSTATE)
+        self.assertIsNone(server.image_uuid, None)
+
+    @mock.patch.object(engine_api.API, 'list_availability_zones')
+    @mock.patch.object(manager.EngineManager, '_rollback_servers_quota')
+    @mock.patch.object(IronicDriver, 'unadopt')
+    @mock.patch.object(IronicDriver, 'adopt')
+    @mock.patch.object(network_api.API, 'show_port')
+    @mock.patch.object(report_api, 'put_allocations')
+    @mock.patch.object(IronicDriver, 'get_manageable_node')
+    def test_manage_servers_with_az_exception(self, get_manageable_mock,
+                                              put_allocations_mock,
+                                              show_port_mock,
+                                              adopt_mock,
+                                              unadopt_mock,
+                                              rollback_quota_mock,
+                                              list_az_mock):
+        neutron_port_id = '67ec8e86-d77b-4729-b11d-a009864d289d'
+        neutron_mac_address = '52:54:00:8e:6a:03'
+        node_uuid = 'aacdbd78-d670-409e-95aa-ecfcfb94fee2'
+        image_uuid = 'efe0a06f-ca95-4808-b41e-9f55b9c5eb98'
+
+        node = {
+            'uuid': node_uuid,
+            'name': 'test_manageable_mode',
+            'resource_class': 'gold',
+            'power_state': 'power on',
+            'provision_state': 'active',
+            "ports": [
+                {
+                    "address": neutron_mac_address,
+                    "uuid": "1ec01153-685a-49b5-a6d3-45a4e7dddf53",
+                    "neutron_port_id": neutron_port_id
+                }
+            ],
+            "portgroups": [
+                {
+                    "address": "a4:dc:be:0e:82:a6",
+                    "uuid": "1ec01153-685a-49b5-a6d3-45a4e7dddf54",
+                    "neutron_port_id": None
+                }
+            ],
+            'image_source': image_uuid,
+            'availability_zone': 'az3'
+        }
+
+        list_az_mock.return_value = {'availability_zones': ['az1', 'az2']}
+        get_manageable_mock.return_value = node
+        put_allocations_mock.side_effect = None
+        show_port_mock.return_value = {
+            'id': neutron_port_id,
+            'network_id': '34ec8e86-d77b-4729-b11d-a009864d3456',
+            'mac_address': neutron_mac_address,
+            'fixed_ips': [{"subnet_id": "d2d7a7c2-17d2-4268-906d-1da8dde24fa8",
+                           "ip_address": "10.80.20.12"}]
+        }
+
+        server = obj_utils.get_test_server(
+            self.context, status=None, node_uuid=None,
+            power_state=states.NOSTATE, availability_zone=None,
+            image_uuid=None)
+
+        adopt_mock.side_effect = None
+        unadopt_mock.side_effect = None
+        self.assertRaises(exception.AZNotFound, self.service.manage_server,
+                          self.context, server, node_uuid)
+        put_allocations_mock.assert_called_once()
+        rollback_quota_mock.assert_called_once()
+        adopt_mock.assert_called_once()
+        unadopt_mock.assert_called_once()
+        show_port_mock.assert_called_once_with(self.context, neutron_port_id)
+        self.assertIsNone(server.node_uuid)
+        self.assertIsNone(server.availability_zone, None)
+        self.assertIsNone(server.status)
+        self.assertEqual(server.power_state, states.NOSTATE)
+        self.assertIsNone(server.image_uuid)
