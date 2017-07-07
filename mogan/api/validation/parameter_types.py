9d65a25935290186ce729c933572f2f3cb9cd417
Tao Li <litao3721@126.com>
2017-07-07 16:56:38 +0800

    Manage existing BMs: Part-2
    Add the adopt baremetal node API.
    
    Change-Id: I1897129930bface6a6c4a179e02d6107ff3811af
    Implements: bp manage-existing-bms

--- mogan/api/validation/parameter_types.py
+++ mogan/api/validation/parameter_types.py
@@ -62,6 +62,10 @@ server_group_id = {
     'type': 'string', 'format': 'uuid'
 }
 
+node_uuid = {
+    'type': 'string', 'format': 'uuid'
+}
+
 metadata = {
     'type': 'object',
     'patternProperties': {
