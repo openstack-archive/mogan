85958ac336f45a2926db0b55f2cbd819d0635cde
liusheng <liusheng@huawei.com>
2017-07-26 17:54:13 +0800

    Improve resources updating process
    This change ensure allocations of a rp being deleted firstly before the rp
    deleted. And this change also avoid updating the inventory of a rp if the
    rp(node) has already been consumed.
    
    Change-Id: I507176834f8247d112c8e6d969656d1a4dded6d7

--- mogan/engine/manager.py
+++ mogan/engine/manager.py
@@ -101,10 +101,23 @@ class EngineManager(base_manager.BaseEngineManager):
         # Clean orphan resource providers in placement
         for rp in all_rps:
             if rp['uuid'] not in node_uuids:
+                server_by_node = objects.Server.list(
+                    context, filters={'node_uuid': rp['uuid']})
+                if not server_by_node:
+                    self.scheduler_client.reportclient.\
+                        delete_allocations_for_resource_provider(rp['uuid'])
                 self.scheduler_client.reportclient.delete_resource_provider(
                     rp['uuid'])
 
         for node in all_nodes:
+            if (node.provision_state == 'active' and
+                    node.instance_uuid is not None):
+                LOG.info("Skip updating resources for node: %s, it was "
+                         "already provisioned", node.uuid)
+                continue
+            if node.provision_state == 'available':
+                self.scheduler_client.reportclient \
+                    .delete_allocations_for_resource_provider(node.uuid)
             resource_class = sched_utils.ensure_resource_class_name(
                 node.resource_class)
             inventory = self.driver.get_node_inventory(node)
@@ -112,9 +125,6 @@ class EngineManager(base_manager.BaseEngineManager):
             self.scheduler_client.set_inventory_for_provider(
                 node.uuid, node.name or node.uuid, inventory_data,
                 resource_class)
-            if node.provision_state == 'available':
-                self.scheduler_client.reportclient \
-                    .delete_allocations_for_resource_provider(node.uuid)
 
     @periodic_task.periodic_task(spacing=CONF.engine.sync_power_state_interval,
                                  run_immediately=True)
