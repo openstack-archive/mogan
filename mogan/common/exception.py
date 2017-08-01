6c314d0efadae613b23b2de70bda36e93a82e332
wanghao <sxmatch1986@gmail.com>
2017-08-01 11:24:40 +0800

    Introduce Cinder client into Mogan
    Introduce the Cinder client into Mogan for
    further features supporting.
    
    Change-Id: Ie972bc6b68e7c0800709117847b184ad1408f36e

--- mogan/common/exception.py
+++ mogan/common/exception.py
@@ -448,4 +448,39 @@ class CannotDisassociateAutoAssignedFloatingIP(Forbidden):
 class FloatingIpNotAssociated(Invalid):
     _msg_fmt = _("Floating IP: %(floatingip)s is not associated")
 
+
+class CinderAPIVersionNotAvailable(Invalid):
+    """Used to indicate that a requested Cinder API version, generally a
+    microversion, is not available.
+    """
+    msg_fmt = _('Cinder API version %(version)s is not available.')
+
+
+class UnsupportedCinderAPIVersion(Invalid):
+    msg_fmt = _('Mogan does not support Cinder API version %(version)s')
+
+
+class CinderConnectionFailed(MoganException):
+    msg_fmt = _("Connection to cinder host failed: %(reason)s")
+
+
+class InvalidInput(Invalid):
+    msg_fmt = _("Invalid input received: %(reason)s")
+
+
+class VolumeNotFound(NotFound):
+    msg_fmt = _("Volume %(volume_id)s could not be found.")
+
+
+class VolumeAttachmentNotFound(NotFound):
+    msg_fmt = _("Volume attachment %(attachment_id)s could not be found.")
+
+
+class InvalidVolume(Invalid):
+    msg_fmt = _("Invalid volume: %(reason)s")
+
+
+class VolumeUnattached(Invalid):
+    msg_fmt = _("Volume %(volume_id)s is not attached to anything")
+
 ObjectActionError = obj_exc.ObjectActionError
