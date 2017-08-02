a5f19421dc589d3ed8cf08f3c1e9a4c0c540cd53
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2017-08-02 11:08:31 +0800

    Add checks for aggregate availability_zone
    * Do not allow empty availablity zone
    * A node can not be in different availablity zone
    
    Change-Id: I82096c60f9d871e5f3ddcbc905dfe62f70b19b79

--- mogan/engine/rpcapi.py
+++ mogan/engine/rpcapi.py
@@ -111,3 +111,7 @@ class EngineAPI(object):
         cctxt = self.client.prepare(topic=self.topic, server=CONF.host)
         return cctxt.call(context, 'remove_aggregate_node',
                           aggregate_uuid=aggregate_uuid, node=node)
+
+    def list_node_aggregates(self, context, node):
+        cctxt = self.client.prepare(topic=self.topic, server=CONF.host)
+        return cctxt.call(context, 'list_node_aggregates', node=node)
