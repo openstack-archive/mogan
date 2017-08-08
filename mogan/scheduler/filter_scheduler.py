f964b690391fa7d6e4dcb92ff62cc4cd541151b7
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2017-08-08 15:08:06 +0800

    Add aggregates filters when do scheduling
    We should extract resources aggregates information from flavor,
    and pass it to placement with resources filters.
    
    Partially Implements: bp node-aggregate
    
    Change-Id: I96eeb7ab6636fa241e7d82bbddad8d70791c1f43

--- mogan/scheduler/filter_scheduler.py
+++ mogan/scheduler/filter_scheduler.py
@@ -92,13 +92,61 @@ class FilterScheduler(driver.Scheduler):
                           for res in flavor_dict['resources'].items()])
         return resources
 
-    def _get_filtered_nodes(self, request_spec):
-        query_filters = {'resources': self._get_res_cls_filters(request_spec)}
-        filtered_nodes = self.reportclient.get_filtered_resource_providers(
-            query_filters)
-        if not filtered_nodes:
+    @staticmethod
+    def _get_res_aggregates_filters(context, request_spec):
+        flavor_dict = request_spec['flavor']
+        resource_aggregates = flavor_dict.get('resource_aggregates', {})
+        resource_aggregates_items = resource_aggregates.items()
+        # Add availability_zone aggregate
+        # resource_aggregates_items.append(('availability_zone',
+        #                                   request_spec['availability_zone']))
+
+        filters = []
+        for key, value in resource_aggregates_items:
+            aggregates = objects.AggregateList.get_by_metadata(
+                context, key, value)
+            if not aggregates:
+                # if no aggregates match with the key/value,
+                # fail the scheduling.
+                return None
+            filters.append([agg.uuid for agg in aggregates])
+
+        return filters
+
+    def _get_filtered_nodes(self, context, request_spec):
+        resources_filter = self._get_res_cls_filters(request_spec)
+        aggs_filters = self._get_res_aggregates_filters(context, request_spec)
+
+        # None indicates no matching aggregates
+        if aggs_filters is None:
             return []
-        return [node['uuid'] for node in filtered_nodes]
+
+        if aggs_filters:
+            filtered_nodes = set()
+            for agg_filter in aggs_filters:
+                query_filters = {'resources': resources_filter,
+                                 'member_of': 'in:' + ','.join(agg_filter)}
+                filtered_rps = self.reportclient.\
+                    get_filtered_resource_providers(query_filters)
+                if not filtered_rps:
+                    # if got empty, just break here.
+                    return []
+                filtered_rps = set([rp['uuid'] for rp in filtered_rps])
+                if not filtered_nodes:
+                    # initialize the filtered_nodes
+                    filtered_nodes = filtered_rps
+                else:
+                    filtered_nodes &= filtered_rps
+                if not filtered_nodes:
+                    # if got empty, just break here.
+                    return []
+        else:
+            query_filters = {'resources': resources_filter}
+            filtered_nodes = self.reportclient.\
+                get_filtered_resource_providers(query_filters)
+            return [node['uuid'] for node in filtered_nodes]
+
+        return list(filtered_nodes)
 
     def schedule(self, context, request_spec, filter_properties=None):
 
@@ -110,7 +158,7 @@ class FilterScheduler(driver.Scheduler):
         @utils.synchronized('schedule')
         def _schedule(self, context, request_spec, filter_properties):
             self._populate_retry(filter_properties, request_spec)
-            filtered_nodes = self._get_filtered_nodes(request_spec)
+            filtered_nodes = self._get_filtered_nodes(context, request_spec)
             if not filtered_nodes:
                 LOG.warning('No filtered nodes found for server '
                             'with properties: %s',
