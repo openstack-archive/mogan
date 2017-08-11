07cac1ac913b8e6f322bba5af35ca517f6754e46
wanghao <sxmatch1986@gmail.com>
2017-08-11 17:11:22 +0800

    Add some missing logs in Mogan
    Now Mogan will release in Pike, there are some
    logs missing in some operations,
    those should be added for reliability.
    
    Change-Id: I90df3a3a5bda67ea6dae8400d37d7b90d744c429
    Closes-Bug: #1710107

--- mogan/engine/api.py
+++ mogan/engine/api.py
@@ -329,7 +329,6 @@ class API(object):
 
         Returns a server object
         """
-
         # check availability zone
         if availability_zone:
             azs = self.list_availability_zones(context)
@@ -386,6 +385,7 @@ class API(object):
     @check_server_maintenance
     def rebuild(self, context, server, image_uuid=None):
         """Rebuild a server."""
+        LOG.debug("Going to try to rebuild server %s", server.uuid)
         if not image_uuid:
             image_uuid = server.image_uuid
         # check if the image exists
@@ -420,7 +420,7 @@ class API(object):
         if server.locked and is_owner:
             return
 
-        LOG.debug('Locking', server=server)
+        LOG.debug('Locking server: %s', server.uuid)
         server.locked = True
         server.locked_by = 'owner' if is_owner else 'admin'
         server.save()
@@ -428,7 +428,7 @@ class API(object):
     def unlock(self, context, server):
         """Unlock the given server."""
 
-        LOG.debug('Unlocking', server=server)
+        LOG.debug('Unlocking server: %s', server.uuid)
         server.locked = False
         server.locked_by = None
         server.save()
@@ -494,6 +494,7 @@ class API(object):
     def create_key_pair(self, context, user_id, key_name,
                         key_type=keypair_obj.KEYPAIR_TYPE_SSH):
         """Create a new key pair."""
+        LOG.debug('Going to create key pair')
         self._validate_new_key_pair(context, user_id, key_name, key_type)
         private_key, public_key, fingerprint = self._generate_key_pair(
             user_id, key_type)
@@ -528,6 +529,7 @@ class API(object):
 
     def delete_key_pair(self, context, user_id, key_name):
         """Delete a keypair by name."""
+        LOG.debug('Going to delete key pair')
         objects.KeyPair.destroy_by_name(context, user_id, key_name)
         reserve_opts = {'keypairs': -1}
         reservations = self.quota.reserve(context, **reserve_opts)
@@ -544,10 +546,12 @@ class API(object):
 
     @check_server_lock
     def attach_interface(self, context, server, net_id):
+        LOG.debug('Going to attach interface to server %s', server.uuid)
         self.engine_rpcapi.attach_interface(context, server, net_id)
 
     @check_server_lock
     def detach_interface(self, context, server, port_id):
+        LOG.debug('Going to detach interface from server %s', server.uuid)
         self.engine_rpcapi.detach_interface(context, server=server,
                                             port_id=port_id)
 
@@ -562,18 +566,23 @@ class API(object):
 
     def add_aggregate_node(self, context, aggregate_uuid, node):
         """Add a node to the aggregate."""
+        LOG.debug('Going to add node %(node)s to aggregate %(aggregate)s',
+                  {'node': node, 'aggregate': aggregate_uuid})
         return self.engine_rpcapi.add_aggregate_node(context,
                                                      aggregate_uuid,
                                                      node)
 
     def remove_aggregate_node(self, context, aggregate_uuid, node):
         """Remove a node to the aggregate."""
+        LOG.debug('Going to remove node %(node) from aggregate %(aggregate)s',
+                  {'node': node, 'aggregate': aggregate_uuid})
         return self.engine_rpcapi.remove_aggregate_node(context,
                                                         aggregate_uuid,
                                                         node)
 
     def remove_aggregate(self, context, aggregate_uuid):
         """Remove the aggregate."""
+        LOG.debug('Going to remove aggregate %s', aggregate_uuid)
         return self.engine_rpcapi.remove_aggregate(context, aggregate_uuid)
 
     def list_node_aggregates(self, context, node):
