9b6e5870a34a982f4f9c76536392e20fdb40aadf
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2017-08-23 17:16:02 +0800

    Leverage _detach_interface for destroying networks
    During network task revert, we should unplug vifs as well.
    
    Change-Id: I26b1ba8ffba167683613d901bcbee65fde1f4b2d
    Closes-Bug: #1712511

--- mogan/engine/manager.py
+++ mogan/engine/manager.py
@@ -284,9 +284,8 @@ class EngineManager(base_manager.BaseEngineManager):
 
     def destroy_networks(self, context, server):
         ports = server.nics.get_port_ids()
-        server.nics.delete(context)
         for port in ports:
-            self.network_api.delete_port(context, port, server.uuid)
+            self._detach_interface(context, server, port)
 
     def _rollback_servers_quota(self, context, number):
         reserve_opts = {'servers': number}
@@ -432,8 +431,6 @@ class EngineManager(base_manager.BaseEngineManager):
                 LOG.error("Destroy networks for server %(uuid)s failed. "
                           "Exception: %(exception)s",
                           {"uuid": server.uuid, "exception": e})
-        for vif in server.nics:
-            self.driver.unplug_vif(context, server, vif['port_id'])
         self.driver.destroy(context, server)
 
     @wrap_server_fault
@@ -555,9 +552,7 @@ class EngineManager(base_manager.BaseEngineManager):
             raise exception.InterfaceAttachFailed(message=six.text_type(e))
         LOG.info('Attaching interface successfully')
 
-    def detach_interface(self, context, server, port_id):
-        LOG.debug("Detaching interface %(port_id) from server %(server)s",
-                  {'port_id': port_id, 'server': server})
+    def _detach_interface(self, context, server, port_id):
         try:
             self.driver.unplug_vif(context, server, port_id)
         except exception.MoganException as e:
@@ -571,9 +566,15 @@ class EngineManager(base_manager.BaseEngineManager):
             except Exception as e:
                 raise exception.InterfaceDetachFailed(server_uuid=server.uuid)
 
-        for nic in server.nics:
-            if nic.port_id == port_id:
-                nic.delete(context)
+        try:
+            objects.ServerNic.delete_by_port_id(context, port_id)
+        except exception.PortNotFound:
+            pass
+
+    def detach_interface(self, context, server, port_id):
+        LOG.debug("Detaching interface %(port_id) from server %(server)s",
+                  {'port_id': port_id, 'server': server.uuid})
+        self._detach_interface(context, server, port_id)
 
         LOG.info('Interface was successfully detached')
 
